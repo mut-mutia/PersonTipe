3304c200a2876272a57b052b30ed7c9f26ab4047
unknown <mut-mutia>
2013-09-20 12:21:01 +0700

    first commit

---
+++ TipeData.java
@@ -0,0 +1,27 @@
+class TipeData
+{
+
+		private String nama;
+		public static void main(String[] mutia)
+		{
+			TipeData isi = new TipeData();
+			isi.setNama("Mutia");
+			byte a = 100;
+			short b = 100;
+			
+			System.out.println("Menampilkan nama = "+isi.getNama());
+			System.out.println("Hasil dari a + b = "+(a + b));
+			
+		}
+		
+		public void setNama(String aNama)
+			{
+			this.nama = aNama;
+			}
+		
+		public String getNama() 
+		{
+		return this.nama;
+		}
+		
+}
---
+++ person.java
@@ -0,0 +1,11 @@
+class person
+{
+	public static void main(String[] mutia)
+	{
+	int x=10;
+	char y;
+	y='A';
+		System.out.println("Menampilkan variable x "+x);
+		System.out.println("Menampilkan variable y "+y);
+	}
+}
